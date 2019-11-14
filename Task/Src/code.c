35f9b2032ed5b177b7b6b0156ecbbc77e2d03371
Hosam-ali <hosam_ali93@hotmail.com>
2019-11-14 22:45:56 +0200

    first version of makefile

---
+++ Task/Src/code.c
@@ -0,0 +1,17 @@
+#include "code.h"
+
+void printMessage()
+{
+	int res;
+	printf("please solve the following quiz:\n");
+	printf("what is the solution for 3*(3+(10/2)\n");
+	scanf("%d",&res);
+	if ( 24 == res)
+	{
+		printf("correct result=%d",res);
+	}
+	else
+	{
+		printf("Wrong answer good luck next time");
+	}
+}
